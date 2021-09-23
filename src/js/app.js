d304a834e2f885910fb696e5cda1442e85199388
Aniket <janiket338@gmail.com>
2021-09-23 14:10:01 +0530

    Added working hamburger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,6 @@
-console.log('This is working!!')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const primaryNav = document.querySelector('.primary-nav')
+
+hamburgerMenu.addEventListener('click', () => {
+    primaryNav.classList.toggle('active')
+})
