434d5cad932a5c2631044dfc992a7152d295c88f
Aniket <janiket338@gmail.com>
2021-10-18 20:20:34 +0530

    Ableton project completed

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,16 @@ const hamburgerMenu = document.querySelector('.hamburger-menu')
 const primaryNav = document.querySelector('.primary-nav')
 const headerTwo = document.querySelector('header:nth-child(2)')
 const headerTwoTopPos = headerTwo.getBoundingClientRect().top
+const SCROLL_UP_SHOW = -20
+const SCROLL_DOWN_HIDE = 50
 let prevScrollPos = window.scrollY
-
-console.log(headerTwoTopPos)
 
 hamburgerMenu.addEventListener('click', () => {
     primaryNav.classList.toggle('active')
 })
 
+
+// Navbar shows/hides when scrolled specified pixels
 window.addEventListener('scroll', () => {
     let currentScrollPos = window.scrollY
 
@@ -20,23 +22,17 @@ window.addEventListener('scroll', () => {
 
     console.log('running3')
     console.log(prevScrollPos, currentScrollPos, currentScrollPos - prevScrollPos)
-    headerTwo.classList.add('show')
-    headerTwo.classList.remove('hide')
 
-    if (currentScrollPos - prevScrollPos > 100) {
+    if (currentScrollPos - prevScrollPos >= SCROLL_DOWN_HIDE) {
         console.log('running1')
         headerTwo.classList.add('hide')
         headerTwo.classList.remove('show')
-
-        return
     }
 
-    if (currentScrollPos - prevScrollPos < -50) {
+    if (currentScrollPos - prevScrollPos < SCROLL_UP_SHOW || prevScrollPos < SCROLL_DOWN_HIDE) {
         console.log('running2')
         headerTwo.classList.add('show')
         headerTwo.classList.remove('hide')
-
-        return
     }
 
     prevScrollPos = currentScrollPos
